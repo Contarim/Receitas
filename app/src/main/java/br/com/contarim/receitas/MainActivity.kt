8ec7744a41c6b1f8a25baba002c3b7b2a415c141
João Contarim <joaocontar@hotmail.com>
2026-03-10 20:35:07 -0300

    Criação da primeira tela

--- app/src/main/java/br/com/contarim/receitas/MainActivity.kt
+++ app/src/main/java/br/com/contarim/receitas/MainActivity.kt
@@ -1,15 +1,19 @@
 package br.com.contarim.receitas
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.contarim.receitas.ui.theme.ReceitasTheme
 
@@ -19,29 +23,33 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ReceitasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
     }
-}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+fun InitialScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Cyan)
+    ){
+        Image(
+            painter = painterResource(br.com.contarim.receitas.R.drawable.chef),
+            contentDescription = "Imagem de um Chef de cozinha.",
+            modifier = Modifier
+                .align(alignment = Alignment.Center)
+        )
+
+    }
+}
 @Composable
-fun GreetingPreview() {
-    ReceitasTheme {
-        Greeting("Android")
+@Preview
+fun initialScreenPreview(){
+    ReceitasTheme() {
+        InitialScreen()
     }
 }
