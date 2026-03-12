f8637caf2e99ee1c2211f50137a6cf0b9eb77bfb
João Contarim <joaocontar@hotmail.com>
2026-03-12 14:15:50 -0300

    Reestruturação do arquivo Theme

--- app/src/main/java/br/com/contarim/receitas/MainActivity.kt
+++ app/src/main/java/br/com/contarim/receitas/MainActivity.kt
@@ -1,14 +1,11 @@
 package br.com.contarim.receitas
 
-import android.R
 import android.os.Bundle
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,7 +16,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
